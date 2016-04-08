42d140d7ee8dfe2eb3ba37b646194c9b102a3b49
Alexis Nugon <alexis.nugon@gmail.com>
2016-04-08 16:59:44 +0200

    alpha_char, more logic to do when keypress

--- app/assets/javascripts/sudoku.js
+++ app/assets/javascripts/sudoku.js
@@ -2,7 +2,9 @@ $(function(){
 	// Limit entered values to numbers
 	$('.sudoku-grid input.cell').keypress(function(e) {
         // Ensure that it is a number and stop the keypress
-        if (e.charCode < 48 || e.charCode > 57)
-            e.preventDefault();
+       evt = (e) ? e : window.event;
+       var charCode = (e.which) ? e.which : e.keyCode;
+       if (charCode != 44 && charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+           return e.preventDefault();
 	});
 });
