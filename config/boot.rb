4a98183ea7fdb2bcb63b8ac2a286f052c0d6d9b0
Alexis Nugon <alexis.nugon@gmail.com>
2016-04-28 16:52:36 +0200

    Config: ExecJS using NodeJS only on WINDOWS to process jsx files

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+# [ONLY Windows] Patch to make JSX transformation possible with NodeJS
+host_os = RbConfig::CONFIG['host_os']
+if /mswin|msys|mingw|cygwin|bccwin|wince|emc/ === host_os
+	ENV['EXECJS_RUNTIME'] = 'Node'
+end
